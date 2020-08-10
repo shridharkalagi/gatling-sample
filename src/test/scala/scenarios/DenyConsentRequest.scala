b38044cd731b3d0263a53ef867fee53827e1cbb1
sharath2106 <gopi.shanky@gmail.com>
2020-08-10 13:07:33 +0530

    add type definitions and change username environment variable to use dummy number account

--- src/test/scala/scenarios/DenyConsentRequest.scala
+++ src/test/scala/scenarios/DenyConsentRequest.scala
@@ -1,6 +1,7 @@
 package scenarios
 
 import io.gatling.core.Predef._
+import io.gatling.core.structure.ChainBuilder
 import io.gatling.http.Predef._
 import io.gatling.http.request.builder.HttpRequestBuilder.toActionBuilder
 import utils.Environment
@@ -8,12 +9,12 @@ import utils.Environment
 
 object DenyConsentRequest {
 
-  val loginRequestBody = "{\n\t\"username\": \"admin\",\n\t\"password\": \"password\"}"
-  val createConsentRequestBody = "{\n    \"consent\": {\n        \"patient\": {\n            \"id\": \"" + Environment.username + "\"\n        },\n        \"purpose\": {\n            \"code\": \"CAREMGT\"\n        },\n        \"hiTypes\": [\n            \"OPConsultation\"\n        ],\n        \"permission\": {\n            \"dateRange\": {\n                \"from\": \"1992-04-03T10:05:26.352Z\",\n                \"to\": \"2020-08-08T10:05:26.352Z\"\n            },\n            \"dataEraseAt\": \"2020-10-30T12:30:00.352Z\"\n        }\n    }\n}"
-  val userRequestBody = "{\"grantType\":\"password\",\"password\":\"Test@1324\",\"username\":\"navjot60@ndhm\"}"
+  val loginRequestBody: String = "{\n\t\"username\": \"admin\",\n\t\"password\": \"password\"}"
+  val createConsentRequestBody: String = "{\n    \"consent\": {\n        \"patient\": {\n            \"id\": \"" + Environment.username + "\"\n        },\n        \"purpose\": {\n            \"code\": \"CAREMGT\"\n        },\n        \"hiTypes\": [\n            \"OPConsultation\"\n        ],\n        \"permission\": {\n            \"dateRange\": {\n                \"from\": \"1992-04-03T10:05:26.352Z\",\n                \"to\": \"2020-08-08T10:05:26.352Z\"\n            },\n            \"dataEraseAt\": \"2020-10-30T12:30:00.352Z\"\n        }\n    }\n}"
+  val userRequestBody: String = "{\"grantType\":\"password\",\"password\":\"Test@1324\",\"username\":\"navjot60@ndhm\"}"
 
 
-  val hiuUserLogin = exec(
+  val hiuUserLogin: ChainBuilder = exec(
     http("create session")
       .post("/sessions")
       .body(StringBody(loginRequestBody))
@@ -21,14 +22,14 @@ object DenyConsentRequest {
       .check(jsonPath("$.accessToken").findAll.saveAs("accessToken"))
   )
 
-  val fetchPatientInfo = exec(
+  val fetchPatientInfo: ChainBuilder = exec(
     http("patient info")
       .get("/v1/patients/" + Environment.username + "?")
       .header("Authorization", "${accessToken}")
       .check(status.is(200))
   )
 
-  val createConsentRequest = exec(
+  val createConsentRequest: ChainBuilder = exec(
     http("create consent request")
       .post("/v1/hiu/consent-requests")
       .header("Authorization", "${accessToken}")
@@ -36,7 +37,7 @@ object DenyConsentRequest {
       .check(status.is(202))
   )
 
-  val getConsentRequestId = exec(
+  val getConsentRequestId: ChainBuilder = exec(
     http("get consent request id")
       .get("/v1/hiu/consent-requests")
       .header("Authorization", "${accessToken}")
@@ -44,7 +45,7 @@ object DenyConsentRequest {
       .check(jsonPath("$..[0].consentRequestId").findAll.saveAs("request"))
   )
 
-  val userLogin = exec(
+  val userLogin: ChainBuilder = exec(
     http("create session")
       .post("/sessions")
       .body(StringBody(userRequestBody))
@@ -52,7 +53,7 @@ object DenyConsentRequest {
       .check(jsonPath("$.token").findAll.saveAs("userAccessToken"))
   )
 
-  val grantHIUConsentRequest = exec(
+  val grantHIUConsentRequest: ChainBuilder = exec(
     http("grant consent request")
       .post("/consent-requests/\"${consentId}\"/deny")
       .header("Authorization", "${userAccessToken}")
--- src/test/scala/utils/Environment.scala
+++ src/test/scala/utils/Environment.scala
@@ -6,6 +6,6 @@ object Environment {
 //  val nhaBaseUrl = scala.util.Properties.envOrElse("baseURL", "https://dev.ndhm.gov.in/api-hiu")
   //    .baseUrl("http://uat.ndhm.gov.in/cm") //UAT
   //    .baseUrl("https://ncg-dev.projecteka.in/consent-manager")   //NCG
-  val username = scala.util.Properties.envOrElse("username", "sharath@ndhm")
+  val username = scala.util.Properties.envOrElse("username", "navjot60@ndhm")
   val password = scala.util.Properties.envOrElse("password", "Test@1324")
 }
