8d273d026062353f5174bf34854d09186e051a4c
msharma-tw <monika.sharma@thoughtworks.com>
2020-08-11 00:39:38 +0530

    auto-consent-request-creation test added

---
+++ src/test/scala/scenarios/AutoCreateConsentRequest.scala
@@ -0,0 +1,77 @@
+package scenarios
+
+import io.gatling.core.Predef._
+import io.gatling.core.structure.{ChainBuilder, ScenarioBuilder}
+import io.gatling.http.Predef._
+import io.gatling.http.request.builder.HttpRequestBuilder.toActionBuilder
+import utils.Constants._
+import utils.Environment
+
+
+object AutoCreateConsentRequest {
+
+  val userRequestBody = "{\"grantType\":\"password\",\"password\":\"" + Environment.password + "\",\"username\":\"" + Environment.username + "\"}"
+
+  val createConsentRequestBody: String = "{\"hipIds\": [\n" + "  \""+ linkedProvider +"\" \n" + "    ],\n" + "    \"reloadConsent\": true      \n" + "}"
+
+
+  val userLogin: ChainBuilder = exec(
+    http("create session")
+      .post("/cm/sessions")
+      .body(StringBody(userRequestBody))
+      .check(status.is(200))
+      .check(jsonPath("$.token").findAll.saveAs("userAccessToken"))
+  )
+
+  val autoCreateConsentRequest: ChainBuilder = exec(
+    http("auto create consent request")
+      .post("/cm/v1/patient/consent-request")
+      .header("Authorization", "${userAccessToken}")
+      .body(StringBody(createConsentRequestBody))
+      .check(status.is(200))
+      .check(jsonPath("$." + linkedProvider).findAll.saveAs("requestId"))
+      .check(bodyString.saveAs("BODY"))
+        ).exec(session => {
+          val body = session("BODY").as[String]
+          println(body)
+          session
+        }
+  )
+
+  val fetchStatusRequestBody: String = "{\"requestIds\": [\"${requestId}\"]      \n" + "}"
+
+  val fetchHealthDataRequestBody: String = "{\"requestIds\": [\"${requestId}\"]      \n" + "    \"limit\": 10,\n" + "\"offset\": 0   \n" + "}                                          "
+
+  val fetchConsentStatus: ChainBuilder = exec(
+    http("fetch consent-request status")
+      .post("/cm/v1/patient/health-information/status")
+      .header("Authorization", "${userAccessToken}")
+      .body(StringBody(fetchStatusRequestBody))
+      .check(status.is(200))
+      .check(jsonPath("$.status").is("SUCCEEDED"))
+      .check(bodyString.saveAs("BODY"))
+        ).exec(session => {
+          val body = session("BODY").as[String]
+          println(body)
+          session
+        }
+  )
+
+  val fetchHealthData: ChainBuilder = exec(
+    http("fetch health-data")
+      .post("/cm/v1/patient/health-information/fetch")
+      .header("Authorization", "${userAccessToken}")
+      .body(StringBody(fetchHealthDataRequestBody))
+      .check(status.is(200))
+      .check(bodyString.saveAs("BODY"))
+        ).exec(session => {
+          val body = session("BODY").as[String]
+          println(body)
+          session
+        }
+  )
+
+  val autoCreateConsentScenario: ScenarioBuilder =
+    scenario("Auto Create Consent Request from CM and fetch user health-records")
+      .exec(userLogin, autoCreateConsentRequest, fetchConsentStatus, fetchHealthData)
+}
--- src/test/scala/scenarios/GrantConsentRequest.scala
+++ src/test/scala/scenarios/GrantConsentRequest.scala
@@ -9,9 +9,12 @@ import utils.Environment
 
 object GrantConsentRequest {
 
+  val requestId = java.util.UUID.randomUUID.toString
   val loginRequestBody = "{\n\t\"username\": \"admin\",\n\t\"password\": \"password\"}"
   val createConsentRequestBody: String = "{\n    \"consent\": {\n        \"patient\": {\n            \"id\": \"" + Environment.username + "\"\n        },\n        \"purpose\": {\n            \"code\": \"CAREMGT\"\n        },\n        \"hiTypes\": [\n            \"OPConsultation\"\n        ],\n        \"permission\": {\n            \"dateRange\": {\n                \"from\": \"1992-04-03T10:05:26.352Z\",\n                \"to\": \"2020-08-08T10:05:26.352Z\"\n            },\n            \"dataEraseAt\": \"2020-10-30T12:30:00.352Z\"\n        }\n    }\n}"
-  val userRequestBody = "{\"grantType\":\"password\",\"password\":\"Test@1324\",\"username\":\"navjot60@ndhm\"}"
+  val userRequestBody: String = "{\"grantType\":\"password\",\"password\":\"" + Environment.username + "\",\"username\":\"" + Environment.username + "\"}"
+  val grantConsentRequestBody: String = " {\"consents\": [\n" + "       {\n" + "\"careContexts\": [{\"careContextReference\": \"NCP1007\",    \n" + "                    \"patientReference\": \"RVH1002\"      \n" + "                },                                         \n" + "                {                                          \n" + "                    \"careContextReference\": \"RV-MHD-01.17.0024\",    \n" + "                    \"patientReference\": \"RVH1002\"      \n" + "                }                                          \n" + "           ],                                              \n" + "           \"hiTypes\": [                                  \n" + "               \"Prescription\"                            \n" + "           ],                                              \n" + "           \"hip\": {                                      \n" + "               \"id\": \"10000005\"                        \n" + "           },                                              \n" + "           \"permission\": {                               \n" + "               \"accessMode\": \"VIEW\",                   \n" + "               \"dataEraseAt\": \"2020-12-27T10:45:54.688\",    \n" + "               \"dateRange\": {                            \n" + "                   \"from\": \"1992-06-25T18:30:00\",      \n" + "                   \"to\": \"2020-06-26T10:45:54.688\"     \n" + "               },                                          \n" + "               \"frequency\": {                            \n" + "                   \"value\": 1,                           \n" + "                   \"unit\": \"HOUR\",                     \n" + "                   \"repeats\": 0                          \n" + "               }                                           \n" + "           }                                               \n" + "       }                                                   \n" + "   ]                                                       \n" + "}                                                          "
+  val approvePINRequestBody: String = "{\"pin\": \"1234\",\n" + "   \"requestId\": \"" + requestId + "\"" + ",\n" + "   \"scope\": \"consentrequest.approve\" \n" + "}"
 
 
   val hiuUserLogin: ChainBuilder = exec(
@@ -27,7 +30,8 @@ object GrantConsentRequest {
       .get("/api-hiu/v1/hiu/consent-requests")
       .header("Authorization", "${accessToken}")
       .check(status.is(200))
-      .check(jsonPath("$..[0].consentRequestId").findAll.saveAs("request"))
+      .check(jsonPath("$..[0].consentRequestId").is("REQUESTED"))
+      .check(jsonPath("$..[0].consentRequestId").findAll.saveAs("consentRequestId"))
   )
 
   val userLogin: ChainBuilder = exec(
@@ -38,14 +42,23 @@ object GrantConsentRequest {
       .check(jsonPath("$.token").findAll.saveAs("userAccessToken"))
   )
 
-  val grantHIUConsentRequest: ChainBuilder = exec(
+  val userPINAuthorization: ChainBuilder = exec(
+    http("validate approve PIN")
+      .post("/cm/sessions")
+      .body(StringBody(approvePINRequestBody))
+      .check(status.is(200))
+      .check(jsonPath("$.token").findAll.saveAs("pinAccessToken"))
+  )
+
+  val grantConsentRequest: ChainBuilder = exec(
     http("grant consent request")
-      .post("/cm/consent-requests/\"${request}\"/approve")
-      .header("Authorization", "${userAccessToken}")
+      .post("/cm/consent-requests/\"${consentRequestId}\"/approve")
+      .body(StringBody(grantConsentRequestBody))
+      .header("Authorization", "${pinAccessToken}")
       .check(status.is(204))
   )
 
-  val grantConsentRequest: ScenarioBuilder =
+  val grantConsentRequestScenario: ScenarioBuilder =
     scenario("Fetch patient information by HIU")
-      .exec(hiuUserLogin, getConsentRequestId, userLogin, grantHIUConsentRequest)
+      .exec(hiuUserLogin, getConsentRequestId, userLogin, userPINAuthorization, grantConsentRequest)
 }
