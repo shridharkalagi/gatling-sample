0c940fe52d979e0db53757e2106b73961182303d
sharath2106 <gopi.shanky@gmail.com>
2020-08-25 15:43:16 +0530

    replace the existing username

--- src/test/scala/utils/Environment.scala
+++ src/test/scala/utils/Environment.scala
@@ -2,7 +2,7 @@ package utils
 
 object Environment {
   val baseUrl = scala.util.Properties.envOrElse("baseURL", "https://preprod.ndhm.gov.in")
-  val username = scala.util.Properties.envOrElse("username", "20navjot@ndhm")
+  val username = scala.util.Properties.envOrElse("username", "davidxanatos@ndhm")
   val password = scala.util.Properties.envOrElse("password", "Test@1324")
 
   val t_users = Integer.getInteger("users", 1).toInt
