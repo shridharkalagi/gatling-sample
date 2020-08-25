0ad796e2a58b06706c48dfab97aab88bdd622c34
sharath2106 <gopi.shanky@gmail.com>
2020-08-25 08:56:42 +0530

    update scenarios with new request id everytime in DiscoverHIP

--- src/test/scala/scenarios/DiscoverHIPForPatient.scala
+++ src/test/scala/scenarios/DiscoverHIPForPatient.scala
@@ -10,20 +10,20 @@ import utils.Environment._
 
 object DiscoverHIPForPatient {
 
-  val discoverHIPRequest: String = "{\n    \"hip\": {\n        \"id\": \""+LINKED_PROVIDER+"\"\n    },\n    \"requestId\": \""+java.util.UUID.randomUUID.toString+"\"\n}"
-  val userRequestBody = "{\"grantType\":\"password\",\"password\":\""+password+"\",\"username\":\""+username+"\"}"
+  val userRequestBody = "{\"grantType\":\"password\",\"password\":\"" + password + "\",\"username\":\"" + username + "\"}"
+
 
   val userLogin: ChainBuilder = exec(
     http("create session")
       .post("/cm/sessions")
       .body(StringBody(userRequestBody))
       .check(status.is(200))
-      .check(jsonPath("$.token").findAll.saveAs("userAccessToken"))
+      .check(jmesPath("[token] | [0]").saveAs("userAccessToken"))
   )
 
   val providerDetails: ChainBuilder = exec(
     http("discover HIP for the user")
-      .get("/cm/providers/"+LINKED_PROVIDER+"?")
+      .get("/cm/providers/" + LINKED_PROVIDER + "?")
       .header(AUTHORIZATION, "${userAccessToken}")
       .check(status.is(200))
   )
@@ -32,12 +32,16 @@ object DiscoverHIPForPatient {
     http("discover care context for the HIP")
       .post("/cm/v1/care-contexts/discover")
       .header(AUTHORIZATION, "${userAccessToken}")
-      .body(StringBody(discoverHIPRequest))
+      .body(StringBody("{\n    \"hip\": {\n        \"id\": \"" + LINKED_PROVIDER + "\"\n    },\n    \"requestId\": \"" + java.util.UUID.randomUUID.toString + "\"\n}"))
       .check(status.is(200))
   )
 
 
   val discoverHIPScenario: ScenarioBuilder =
     scenario("Fetch patient information by HIU")
-      .exec(userLogin, providerDetails, discoverHIP)
+      .exec(userLogin)
+      .pause(10)
+      .exec(providerDetails)
+      .pause(10)
+      .exec(discoverHIP)
 }
